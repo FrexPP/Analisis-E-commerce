83e5fcc1d126cbfd7eb3a253a81a3ad46e0ec757
FrexPP <franferreyra1616@gmail.com>
2025-10-23 15:52:26 -0300

    Update Clientes de más alto valor.sql

--- Clientes de más alto valor.sql
+++ Clientes de más alto valor.sql
@@ -1,30 +1,20 @@
--- TOP 3 Sub-Categorías que más ingresos generaron por temporada
+-- Clientes de más alto valor
 
--- Con la primer consulta del WITH creo un CTE donde separo las ganancias por temporada y por cada Sub-Categoría.
-WITH Ventas_temporada AS (
-SELECT CASE
-       WHEN substr(O."Order Date",4,2) IN ('10','11','12') THEN 'Primavera'
-       WHEN substr(O."Order Date",4,2) IN ('01','02','03') THEN 'Verano'
-       WHEN substr(O."Order Date",4,2) IN ('04','05','06') THEN 'Otoño'
-       WHEN substr(O."Order Date",4,2) IN ('07','08','09') THEN 'Invierno'
-       END AS Temporada,
-       OD."Sub-Category" AS Sub_categoria,
-       SUM(OD.Quantity) AS Unidades,
-       SUM(OD.Profit) AS Monto
+-- Con un WITH creo un campo en el que guardo el total que generaron todos los empleados en general.
+WITH Ingresos_totales AS (
+SELECT SUM(profit) AS Ingreso_total
+FROM Order_Details
+)
+-- Hago la consulta donde selecciono los clientes y lo que generaron cada uno por separado.
+SELECT O.CustomerName AS Cliente, 
+       CAST(SUM(OD.profit) AS INTEGER) AS Ingreso_generado,
+       COUNT(DISTINCT O."Order ID") AS Ordenes_realizadas,
+-- Utilizo el campo que creé con WITH para sacar el porcentaje de cada cliente.       
+       ROUND((SUM(OD.profit) / IT.Ingreso_total) * 100, 2) ||"%" AS Porcentaje
 FROM List_of_Orders O
 JOIN Order_Details OD ON O."Order ID" = OD."Order ID"
-GROUP BY Temporada, Sub_categoria),
--- En el segundo CTE asigno los TOPS por temporada (Donde si llega a empatar por monto, desempata por unidades vendidas).
-Ranking_temporada AS (
-SELECT Temporada,
-       Sub_categoria,
-       Unidades,
-       Monto,
-       ROW_NUMBER() OVER (
-       PARTITION BY Temporada
-       ORDER BY Monto DESC, Unidades DESC) AS Ranking
-FROM Ventas_temporada)
--- Hago la consulta final dejando solo hasta el TOP 3 por temporada.
-SELECT Temporada, Sub_categoria, CAST(Monto AS INTEGER) AS Monto, Unidades,
-       "TOP #"|| Ranking AS TOPS FROM Ranking_temporada
-WHERE Ranking <= 3
+-- Tengo que unir el total generado de todos los clientes con un CROSS JOIN para que este valor se repita con todos los clientes
+CROSS JOIN Ingresos_totales IT
+GROUP BY O.CustomerName
+ORDER BY Ingreso_generado DESC
+LIMIT 10
